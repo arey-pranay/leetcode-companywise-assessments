e70704a265dc055dd8436d7aa668a6ff92cac764
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-03-08 11:32:50 +0530

    Fix postorder traversal to add root value last

--- 43% Selection (Phone Interview)/Session-3/01. nAryTreePostorder-#590.java
+++ 43% Selection (Phone Interview)/Session-3/01. nAryTreePostorder-#590.java
@@ -1,22 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-}
-*/
-
 class Solution {
     public List<Integer> postorder(Node root) {
         List<Integer> ans = new ArrayList<>();
@@ -25,13 +6,12 @@ class Solution {
         return ans;
     }
     public void func(Node root , List<Integer> ans){
-        ans.add(root.val);
-        
         if(root.children == null){
             ans.add(root.val);
             return;
         }
         for(Node n : root.children) func(n,ans);
+        ans.add(root.val);
         return;
     }
 }
